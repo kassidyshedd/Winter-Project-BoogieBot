4605f461fe14526a7b4cacdf44e52ff5e16d32c4
kassidyshedd <kass92800@gmail.com>
2024-02-18 13:11:20 -0600

    attempt to load action pages

--- state/src/action_edit.cpp
+++ state/src/action_edit.cpp
@@ -2,6 +2,10 @@
 #include "std_msgs/String.h"
 #include "state/RandomList.h"
 #include "op3_action_editor/action_editor.h"
+#include "op3_action_module/action_module.h"
+
+#include <vector>
+#include <fstream>
 
 // Create States
 enum State {
@@ -50,6 +54,30 @@ class ActionEditing
         bool first_message;
         bool reset_message;
 
+        std::vector<int64_t> action_pages;
+
+        // Load action file
+        std::vector<robotis_op::action_file_define::Page> load_action_file(const std::string& filename)
+        {
+            // Open current .bin file
+            std::ifstream file(filename, std::ios::binary);
+            if (!file.is_open())
+            {
+                return {};
+            }
+
+            std::vector<robotis_op::action_file_define::Page> pages;
+            robotis_op::action_file_define::Page page;
+
+            while (file.read(reinterpret_cast<char*>(&page), sizeof(page)))
+            {
+                pages.push_back(page);
+            }
+
+            return pages;
+        }
+
+
         void action_list_callback(const state::RandomList::ConstPtr& msg)
         {
             if (!first_message)
@@ -57,8 +85,10 @@ class ActionEditing
                 ROS_INFO("Action Editing - Recieved message on 'send_list' topic, switch to editing state. ");
                 
                 // Store the list of pages
-                // action_pages = 
+                action_pages = msg->nums;
+                ROS_INFO("Stored Pages");
 
+                std::vector<robotis_op::action_file_define::Page> book = load_action_file('data/motion_4095.bin')
 
                 first_message = true;
                 state = EDITING;
